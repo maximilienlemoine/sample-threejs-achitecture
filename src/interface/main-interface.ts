566c1e740ba8bd0bbd1c3744078241973ed2c5bb
Maximilien LEMOINE <maximilien.lemoine.pro@gmail.com>
2025-04-07 20:43:08 +0200

    feat : add timer

--- src/interface/main-interface.ts
+++ src/interface/main-interface.ts
@@ -1,1 +1,35 @@
+import { Timer } from "./utils/timer.ts";
+import "./styles/timer.css";
+
 console.info("Interface : READY");
+
+const timer = new Timer((time: string) => {
+    const timerElement = document.getElementById("timer");
+    if (!timerElement) return;
+
+    timerElement.innerText = time;
+});
+
+timer.start();
+
+const timer_start = document.getElementById("timer-start");
+const timer_stop = document.getElementById("timer-stop");
+const timer_reset = document.getElementById("timer-reset");
+
+if (timer_start) {
+    timer_start.addEventListener("click", () => {
+        timer.start();
+    });
+}
+
+if (timer_stop) {
+    timer_stop.addEventListener("click", () => {
+        timer.stop();
+    });
+}
+
+if (timer_reset) {
+    timer_reset.addEventListener("click", () => {
+        timer.reset();
+    });
+}
